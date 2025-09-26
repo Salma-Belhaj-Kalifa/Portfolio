db4047e352c1e0badf9ce18b38c8de2b673f4c47
unknown <bhksalma0@gmail.com>
2025-09-26 13:44:57 +0200

    update

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -8,7 +8,7 @@ function AboutCard() {
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
-            Hi Everyone, I am <span className="purple">Salma Belhaj</span> from <span className="purple">Sousse, Tunisia.</span>
+            Hi Everyone, I am <span className="purple">Salma Ben Haj Khalifa</span> from <span className="purple">Sousse, Tunisia.</span>
             <br />
             I am currently a final-year software engineering student at ISSAT Sousse. I am passionate about web development and enjoy building interactive, user-friendly applications that make a positive impact. I am also particularly interested in integrating AI technologies into websites to create smarter, more dynamic user experiences.
           </p>
